cd5e6b52f8b38e0fa49172ac17f15a63f47c41ff
Ben Cochrane <bencochrane1@me.com>
2014-12-07 18:39:43 +1100

    transfer funds to seller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206040208) do
+ActiveRecord::Schema.define(version: 20141207071122) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20141206040208) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "recipient"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
